251f85bed03c4e79b35bc1a9849cf2e015d2e3fd
Jeremy-D-Harris <jeremy.harris31@gmail.com>
2024-07-30 01:26:04 -0400

    sims and plotting for figure 4 speed strength vs. correlation coefficient

--- code_plt/main_plt_Figure_correlations_speed_072924.m
+++ code_plt/main_plt_Figure_correlations_speed_072924.m
@@ -1,11 +1,13 @@
 % function void = main_plt_Figure_correlations_speed(void)
+% function void = main_plt_Figure_correlations_speed_072924(void)
+
 % want to plot correlations vs speed & strength
 
 %%
 
 clear all; close all; clc;
 
-save_fig_ans = 0;
+save_fig_ans = 1;
 % save figure:
 % 0 = no, 1 = yes
 
@@ -21,78 +23,67 @@ depth = 9;
 
 colors_rgb = grad1;
 
-% define ending time:  
-this_t_end_plt = 300;
 
-
-%%
-% load results from file
+%% load results from file
 file_location = '../data/';
 
 
-% results from simulations
-
-
-
-%% (1) positive
+% (1) positive
 this_infile = 'GaussianPositiveCorrelation_0pt6.mat';
 load(strcat(file_location,this_infile));
 % infile_results = 'Gaussian_lowvariance_update072924.mat';
 % load(strcat(file_location,infile_results));
 
+corr_coef(1) = params.corr_coeff;
 R0_collect(1) = results.Rt_traj(1);
 total_incidence_collect(:,1) = results.total_incidence;
 
 
-
-%% (2) positive
+% (2) positive
 this_infile = 'GaussianPositiveCorrelation_0pt3.mat';
 load(strcat(file_location,this_infile));
 
+corr_coef(2) = params.corr_coeff;
 R0_collect(2) = results.Rt_traj(1);
 total_incidence_collect(:,2) = results.total_incidence;
-
 
 
 % (3) independent
 this_infile = 'Gaussian_update072924.mat';
 load(strcat(file_location,this_infile));
 
+% should update?
+corr_coef(3) = 0; %params.corr_coeff;
 R0_collect(3) = results.Rt_traj(1);
 total_incidence_collect(:,3) = results.total_incidence;
-
 
 
 % (4) negative
 this_infile = 'GaussianNegativeCorrelation_0pt3.mat';
 load(strcat(file_location,this_infile));
 
+corr_coef(4) = params.corr_coeff;
 R0_collect(4) = results.Rt_traj(1);
 total_incidence_collect(:,4) = results.total_incidence;
-
 
 
 % (5) negative
 this_infile = 'GaussianNegativeCorrelation_0pt6.mat';
 load(strcat(file_location,this_infile));
 
+corr_coef(5) = params.corr_coeff;
 R0_collect(5) = results.Rt_traj(1);
 total_incidence_collect(:,5) = results.total_incidence;
 
 
-
-
-%% analytic R0
+% analytic R0
 corrcoef_vary = -1:0.05:1;
 
 variance_eps_S = 0.49;
 variance_delta_S = 0.34;
 
-stan_dev_eps_S = sqrt(variance_eps_S);
-stan_dev_delta_S = sqrt(variance_delta_S);
+R0_analytic = (params.bet/params.gam)*(1+corrcoef_vary*variance_eps_S*variance_delta_S);
 
-R0List_analytic = (params.bet/params.gam)*(1+corrcoef_vary*stan_dev_eps_S*stan_dev_delta_S);
-% R0List_analytic = R0List_analytic;
 
 
 
@@ -100,16 +91,56 @@ R0List_analytic = (params.bet/params.gam)*(1+corrcoef_vary*stan_dev_eps_S*stan_d
 f1 = figure(1); set(f1, 'Position', [100 500 900 350]);
 
 
-%%
+%% plot incidence on linear scale
+subplot(1,2,1);
+
+for count = 1:5
+    this_h(count) =plot(params.t_span,total_incidence_collect(:,count),'Color',colors_rgb(2*count-1,:),'LineWidth',2.5); hold on;
+end
+
+axis([0 200 0 0.02]);
+xlabel('Time (days)'); ylabel({'Incident Infections, $\eta(t)$'},'Interpreter','Latex');
+f1=gca;
+f1.LineWidth = 1;
+f1.FontSize = 16;
+f1.FontWeight = 'normal';
+f1.FontName = 'Times';
+
+
+
+txt = {'A'};
+text(0.025,1.035,txt,'Units','normalized','FontSize',18,'FontWeight','bold');
+box on
+
+legend_char1 = strcat('$\rho = \,$',num2str(round(corr_coef(1),2)));
+legend_char2 = strcat('$\rho = \,$',num2str(round(corr_coef(2),2)));
+legend_char3 = strcat('$\rho = \,$',num2str(round(corr_coef(3),2)));
+legend_char4 = strcat('$\rho = \,$',num2str(round(corr_coef(4),2)));
+legend_char5 = strcat('$\rho = \,$',num2str(round(corr_coef(5),2)));
+%legend_char4 = 'Classic SIR';
+
+legend(this_h,{legend_char1,legend_char2,legend_char3,legend_char4,legend_char5}, 'Interpreter','Latex','FontSize',16, 'Location','Northeast');
+
+% end
+
+
+
+
+%% Panel B
 subplot(1,2,2);
 
-if count==1
-    this_q(1) = plot(corrcoef_vary, R0List_analytic,'k--','LineWidth', 2.5); hold on;
-    %         plot(corrcoef, this_R0_analytic,'k.','MarkerSize', 20); hold on;
-end
-plot(corrcoef(:,count), R0_calc(count),'.', 'Color', colors_rgb(2*count-1,:), 'MarkerSize', 30);hold on;
+for count = 1:5
 
-axis([-1 1 1.2 2.8]);
+    if count == 1
+        plot(corrcoef_vary, R0_analytic,'k--','LineWidth', 2.5); hold on;
+        plot(corr_coef(count), R0_collect(count),'.', 'Color', colors_rgb(2*count-1,:), 'MarkerSize', 30);hold on;
+    else
+        plot(corr_coef(count), R0_collect(count),'.', 'Color', colors_rgb(2*count-1,:), 'MarkerSize', 30);hold on;
+    end
+
+end
+
+axis([-1 1 1.5 2.5]);
 xlabel({'Correlation Coefficient, $\rho$'},'Interpreter','Latex');
 ylabel({'Basic Reproduction Number, $\mathcal R_0$'},'Interpreter','Latex');
 
@@ -120,58 +151,14 @@ f1.FontWeight = 'normal';
 f1.FontName = 'Times';
 
 
-if count==5
-    txt = {'B'};
-    text(0.025,1.035,txt,'Units','normalized','FontSize',18,'FontWeight','bold');
-
-
-    % legend_char_q = strcat('Eq. (14), $\sigma_\varepsilon(0) = \sigma_\delta(0) = $',num2str(mean_stand_dev_delta,'%2.2f'));
-    legend_char_q = 'Analytic $\mathcal R_0$';
-    legend(this_q,{legend_char_q}, 'Interpreter','Latex','FontSize',16, 'Location','NorthWest');
-    legend boxoff;
-
-end
-
-
-%% now plot linear scale
-subplot(1,2,1);
-
-this_h(count) =plot(t_shift_plt_finer(count,:),total_incidence_interp(count,:),'Color',colors_rgb(2*count-1,:),'LineWidth',2.5); hold on;
-%     this_h(count).Color(4)=0.8;
-%     semilogy(t_shift_plt_finer(count,ind_t_int(count))*ones(length(t_shift_plt_finer)),...
-%         linspace(10^-4,total_incidence_interp(count,ind_t_int(count)),length(t_shift_plt_finer)),'--','Color',colors_rgb(count,:),'LineWidth',2); hold on;
-axis([0 this_t_end_plt 0 0.025]);
-xlabel('Time (days)'); ylabel({'Incident Infections, $\eta(t)$'},'Interpreter','Latex');
-% ylabel({'Total'; 'Incidence, $i(t)$'},'Interpreter','Latex');
-f1=gca;
-f1.LineWidth = 1;
-f1.FontSize = 16;
-f1.FontWeight = 'normal';
-f1.FontName = 'Times';
-
-
-if count==5
-
-    txt = {'A'};
-    text(0.025,1.035,txt,'Units','normalized','FontSize',18,'FontWeight','bold');
-    box on
-
-    legend_char1 = strcat('$\rho = $',num2str(round(corrcoef(:,1),2)));
-    legend_char2 = strcat('$\rho = $',num2str(round(corrcoef(:,2),2)));
-    legend_char3 = strcat('$\rho = $',num2str(round(corrcoef(:,3),2)));
-    legend_char4 = strcat('$\rho = $',num2str(round(corrcoef(:,4),2)));
-    legend_char5 = strcat('$\rho = $',num2str(round(corrcoef(:,5),2)));
-    %legend_char4 = 'Classic SIR';
-
-    legend(this_h,{legend_char1,legend_char2,legend_char3,legend_char4,legend_char5}, 'Interpreter','Latex','FontSize',16, 'Location','Northeast');
-
-end
+txt = {'B'};
+text(0.025,1.035,txt,'Units','normalized','FontSize',18,'FontWeight','bold');
 
 
 
 if save_fig_ans==1
 
-    figures_location = './figures/';
+    figures_location = '../figures/';
     saveas(f1,strcat(figures_location,figure_name),'epsc');
 
     fprintf('Figure saved:\n'); % want to be close to 25 days in
